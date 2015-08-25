dfe376b1b1e6b1e0eab8b2ccc58bac74a0aa3dc1
Karim El-Allame <k.el-allame@callimedia.fr>
2015-08-25 19:50:16 +0200

    htmlbuild integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-angular-gettext');
+	grunt.loadNpmTasks('grunt-html-build');
 	grunt.initConfig({
 		nggettext_extract: {
 			pot: {
@@ -14,6 +15,46 @@ module.exports = function(grunt) {
 					'translations.js':['po/*.po']
 				}
 			}
-		}
-	})
+		},
+		fixturesPath: "fixtures",
+
+		    htmlbuild: {
+		        dist: {
+		            src: 'index.html',
+		            dest: 'samples/',
+		            options: {
+		                beautify: true,
+		                prefix: '//some-cdn',
+		                relative: true,
+		                scripts: {
+		                    bundle: [
+		                        '<%= fixturesPath %>/scripts/*.js',
+		                        '!**/main.js',
+		                    ],
+		                    main: '<%= fixturesPath %>/scripts/main.js'
+		                },
+		                styles: {
+		                    bundle: [
+		                        '<%= fixturesPath %>/css/libs.css',
+		                        '<%= fixturesPath %>/css/dev.css'
+		                    ],
+		                    test: '<%= fixturesPath %>/css/inline.css'
+		                },
+		                sections: {
+		                    views: '<%= fixturesPath %>/views/**/*.html',
+		                    templates: '<%= fixturesPath %>/templates/**/*.html',
+		                    layout: {
+		                        header: '<%= fixturesPath %>/layout/header.html',
+		                        footer: '<%= fixturesPath %>/layout/footer.html'
+		                    }
+		                },
+		                data: {
+		                    // Data to pass to templates
+		                    version: "0.1.0",
+		                    title: "test",
+		                },
+		            }
+		        }
+	    	}
+	});
 }
